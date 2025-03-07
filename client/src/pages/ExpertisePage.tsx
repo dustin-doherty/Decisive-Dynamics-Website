39ddf193b2b0a6aad8f1744175b77dfbc9c7e287
Connor Dailey <connorldailey@gmail.com>
2025-03-06 20:40:57 -0500

    Add expertise and careers section to homepage

--- client/src/pages/ExpertisePage.tsx
+++ client/src/pages/ExpertisePage.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import '../styles/expertisepage.css';
 
 const ExpertisePage: React.FC = () => {
+    const { type } = useParams<{ type: string }>();
 
     return (
         <>
-            <h1>Expertise Page</h1>
+            <h1>{type ? type.replace(/-/g, ' ') : 'Expertise'}</h1>
+            {/* Render content tailored to the 'type' */}
         </>
     );
 };
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/homepage.css';
 
 // Hero Page Videos
 import hero1 from '../assets/videos/hero1.mp4';
 import hero2 from '../assets/videos/hero2.mp4';
 import hero3 from '../assets/videos/hero3.mp4';
+import joinUsImg from '../assets/images/joinUs.jpg';
 const heroVideos = [hero1, hero2, hero3];
 
 const HomePage: React.FC = () => {
@@ -60,7 +62,7 @@ const HomePage: React.FC = () => {
                 />
             </section>
 
-            {/* Content */}
+            {/* Impact Section */}
             <section className='impact-section'>
                 <div className="impact-section__headline">
                     <h2>Dynamic solutions.</h2>
@@ -73,6 +75,48 @@ const HomePage: React.FC = () => {
                         solutions our clients need.
                     </p>
                 </div>
+            </section>
+
+            {/* Expertise Section */}
+            <section className='expertise-section'>
+                    <h2 className='expertise-section__headline'>Expertise</h2>
+                    <p className='expertise-section__subheadline'>$200M+ Projects & Programs Managed</p>
+                    <div className='expertise-section__links'>
+                        <div className="expertise-section__links-col1">
+                            <Link to="/expertise/utility-design">Utility Design</Link>
+                            <Link to="/expertise/stormwater-management">Stormwater Management</Link>
+                            <Link to="/expertise/water-and-wastewater">Water and Wastewater</Link>
+                            <Link to="/expertise/parks-and-recreation">Parks and Recreation</Link>
+                            <Link to="/expertise/civil-engineering">Civil Engineering</Link>
+                        </div>
+                        <div className="expertise-section__links-col2">
+                            <Link to="/expertise/due-diligence">Due Diligence</Link>
+                            <Link to="/expertise/planning">Planning</Link>
+                            <Link to="/expertise/transportation-engineering">Transportation Engineering</Link>
+                            <Link to="/expertise/construction-services">Construction Services</Link>
+                        </div>
+                    </div>
+            </section>
+
+            {/* Careers Section */}
+            <section className='careers-section'>
+                    <div className='careers-section__image-content'>
+                        <img 
+                            className='careers-section__image' 
+                            src={joinUsImg} 
+                            alt="Join Us Image" 
+                        />
+                    </div>
+                    <div className='careers-section__text-content'>
+                        <h3 className='careers-section__text-heading'>Looking for a Career?</h3>
+                        <p className='careers-section__text-main'>
+                            Immerse yourself in an environment that elevates your potential 
+                            while empowering you to shape its future.
+                        </p>
+                        <button className='careers-section__button'>
+                            <Link to='careers'> Explore Careers at Decisive Dynamics</Link>
+                        </button>
+                    </div>
             </section>
         </>
     );
